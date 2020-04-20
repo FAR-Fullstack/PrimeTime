da524b7161d499819a28770d487ea1c2d8befa37
Raphaëlle Richard de Latour <meden.agan.rrdl@gmail.com>
2020-04-20 19:30:56 +0200

    past_event and future_event delete, edit

--- app/controllers/past_events_controller.rb
+++ app/controllers/past_events_controller.rb
@@ -3,6 +3,10 @@ class PastEventsController < ApplicationController
     @past_events = PastEvent.all
   end
 
+  def show
+    @past_event = PastEvent.find(params[:id])
+  end
+
   def new
     @past_event = PastEvent.new
   end
@@ -10,16 +14,32 @@ class PastEventsController < ApplicationController
   def create
     @past_event = PastEvent.new(past_event_params)
     if @past_event.save
-      redirect_to :root
+      redirect_to past_events_path, notice: 'successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+    @past_event = PastEvent.find(params[:id])
+  end
+
+  def update
+    @past_event = PastEvent.find(params[:id])
+    @past_event.update(past_event_params)
+    redirect_to past_events_path, notice: 'update done'
+  end
+
+  def destroy
+    @past_event = PastEvent.find(params[:id])
+    @past_event.destroy
+    redirect_to past_events_path, notice: 'successfully delete'
+  end
+
   private
 
   def past_event_params
-    params.require(:past_event).permit(:title, :description, :date_start, :date_end)
+    params.require(:past_event).permit(:title, :description, :date_start, :date_end, :id)
   end
 
 end
