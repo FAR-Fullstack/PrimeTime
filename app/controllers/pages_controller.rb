48a65b3251bde812fb1b8bf56b81691c79e7cbb4
Alexandre Zagame <zagamealexandre@gmail.com>
2020-04-27 19:34:04 +0200

    changing authentification default

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def home
     @past_events = PastEvent.all
     @future_events = FutureEvent.all
   end
+
+  def index
+    @past_events = PastEvent.all
+  end
+
+  def show
+    @past_event = PastEvent.find(params[:id])
+  end
+
 end
