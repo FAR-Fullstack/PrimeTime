020e1aa32e07936090814d7ded57f7b1f1d0b4f0
Alexandre Zagame <zagamealexandre@gmail.com>
2020-04-27 17:48:16 +0200

    working on the event carousel and adding categories to the events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_072337) do
+ActiveRecord::Schema.define(version: 2020_04_27_154525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_072337) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "past_events", force: :cascade do |t|
@@ -54,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_04_23_072337) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   create_table "pictures", force: :cascade do |t|
