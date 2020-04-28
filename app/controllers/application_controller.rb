74559a93dc35f1212da18d932528a2f0c4924ebf
Alexandre Zagame <zagamealexandre@gmail.com>
2020-04-28 15:31:36 +0200

    created a public and private event pages with controller, routes and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:show, :index, :public_events, :private_events]
 
 end
