22b5a9bc8302ef4c01274bd2d24f8bbec188944e
Alexandre Zagame <zagamealexandre@gmail.com>
2020-05-20 11:21:28 +0200

    meta changes for og:image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:show, :index, :public_events, :private_events]
 
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+end
 end
